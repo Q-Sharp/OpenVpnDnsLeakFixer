d7310e14395d68862e8807ba976417e9cfef1206
QoOdrink3r <qoodrink3r@outlook.de>
2020-06-30 21:13:26 +0200

    Add project files.

---
+++ OpenVpnDnsLeakFixer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OpenVpnDnsLeakFixer
+{
+    class Program
+    {
+        static void Main() => Directory.GetFiles(Directory.GetCurrentDirectory()).Where(x => Path.GetExtension(x) == ".ovpn").ToList().ForEach(f =>
+        {
+            if(!File.ReadAllText(f).Contains("block-outside-dns"))
+                File.AppendAllText(f, Environment.NewLine + "block-outside-dns")
+        });
+    }
+}
