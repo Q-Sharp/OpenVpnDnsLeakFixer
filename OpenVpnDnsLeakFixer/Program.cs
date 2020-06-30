3027a7d9866b5841a17bfd34417333b3d8e393da
QoOdrink3r <qoodrink3r@outlook.de>
2020-06-30 21:16:29 +0200

    add missing ;

--- OpenVpnDnsLeakFixer/Program.cs
+++ OpenVpnDnsLeakFixer/Program.cs
@@ -8,8 +8,8 @@ namespace OpenVpnDnsLeakFixer
     {
         static void Main() => Directory.GetFiles(Directory.GetCurrentDirectory()).Where(x => Path.GetExtension(x) == ".ovpn").ToList().ForEach(f =>
         {
-            if(!File.ReadAllText(f).Contains("block-outside-dns"))
-                File.AppendAllText(f, Environment.NewLine + "block-outside-dns")
+            if (!File.ReadAllText(f).Contains("block-outside-dns"))
+                File.AppendAllText(f, Environment.NewLine + "block-outside-dns");
         });
     }
 }
